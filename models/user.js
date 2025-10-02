7720829c57aa47cbdd4eed0c9b4ba8014597f660
Fatema-227 <fat00m.alshughul.2oo5@gmail.com>
2025-10-02 11:28:08 +0300

    index v1

--- models/user.js
+++ models/user.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose')
-<<<<<<< HEAD
 
 const userSchema = new mongoose.Schema({
   username:{
@@ -24,17 +23,3 @@ const userSchema = new mongoose.Schema({
 )
 
 const User = mongoose.model("User", userSchema)
-=======
-const userSchema = new mongoose.Schema(
-  {
-    username: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { type: String, required: true },
-    image: { type: String }
-  },
-  { timestamps: true }
-)
-const User = mongoose.model('User', userSchema)
-
->>>>>>> main
-module.exports = User
