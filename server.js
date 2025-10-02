3b36d32fb5cd75c8035c536099296d9430ee845b
fatima-mohmd <fatoomm473@gmail.com>
2025-10-02 09:43:14 +0300

    server

--- server.js
+++ server.js
@@ -1,18 +1,43 @@
-const express = require('express')
-const path = require('path')
-require('dotenv').config()
+const express=require("express")
+const app=express()
+app.use(express.urlencoded())
 
-const app = express()
+require("dotenv").config()
 
-const logger = require('morgan')
-const methodOverride = require('method-override')
-const session = require('express-session')
+const mongoose=require("./config/db")
 
-//Port configuration
-const port = process.env.PORT ? process.env.PORT : "3000"
+const methodOverride=require("method-override")
+app.use(methodOverride("_method"))
 
+const morgan=require("morgan")
+app.use(morgan("dev"))
 
-app.listen(port, () => {
-    console.log(`The express app is ready on port ${port}`)
-  })
+const session=require("express-session")
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
 
+// const passUserToView=require("./middleware/pas-user-to-view")
+// app.use(passUserToView)
+
+// const isSignedIn = require("./middleware/is-signed-in")
+
+const authRouter=require("./routes/auth")
+app.use("/auth",authRouter)
+
+// const listenRouter=require("./routes/listing")
+// app.use("/listings",isSignedIn, listenRouter)
+
+app.get("/",(req,res)=>{
+  res.render("index.ejs")
+})
+// app.get("/vip-lounge",isSignedIn,(req,res)=>{
+//   res.send(`Welcome to the party ${req.session.user.username}`)
+// })
+
+const port=process.env.PORT ? process.env.PORT:"3000"
+app.listen(port,()=>{
+console.log("app is listening")
+})
