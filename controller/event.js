2f19b60f768ddc155e413355861455954aec1f70
Fatema-227 <fat00m.alshughul.2oo5@gmail.com>
2025-10-03 12:11:21 +0300

    update password

---
+++ controller/event.js
@@ -0,0 +1,16 @@
+const Event = require("../models/event")
+exports.event_index_get = async (req, res) => {
+  const events = await Event.find().populate("user_id") // Using populate owner is to display the owner from the user model
+  res.render("events/index.ejs", { events })
+}
+exports.event_new_get = async (req, res) => {
+  res.render("events/new.ejs")
+}
+exports.event_new_post = async (req, res) => {
+  req.body.user_id = req.session.user._id
+  req.body.isPublic === "on"
+    ? (req.body.isPublic = true)
+    : (req.body.isPublic = false)
+  await Event.create(req.body)
+  res.redirect("/events")
+}
