5f029155a629f258340523239a84f1544d8d3de9
fatima-mohmd <fatoomm473@gmail.com>
2025-10-04 22:59:51 +0300

    orgaized

--- routes/event.js
+++ routes/event.js
@@ -3,4 +3,5 @@ const eventCtrl=require("../controller/event.js")
 router.get("/",eventCtrl.event_index_get)
 router.get("/new",eventCtrl.event_new_get)
 router.post("/",eventCtrl.event_new_post)
+router.get("/:eventID",eventCtrl.app_show_get)
 module.exports=router
