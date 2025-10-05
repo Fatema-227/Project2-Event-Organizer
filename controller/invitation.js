6f1450ce7232aa6f117ad6998ff3c9b9f713a6f0
Fatema-Abdulla <Darksweet202@gmail.com>
2025-10-05 20:07:23 +0300

    select the guests

--- controller/invitation.js
+++ controller/invitation.js
@@ -1,8 +1,13 @@
 const session = require("express-session")
 const Invitation = require("../models/invitation")
+const User = require("../models/user")
 const Event = require("../models/event")
 
 exports.invitation_index_get = async (req, res) => {
-  const eventDetails = await Event.find().populate("user_id")
-  res.render("invitations/index.ejs", {eventDetails})
+  // reference https://stackoverflow.com/questions/4299991/how-to-sort-in-mongoose
+  const users = await User.find({}).sort('username')
+  const currentUser = req.session.user._id
+
+  const allUsers = users.filter((user) => user._id.toString() !== currentUser.toString())
+  res.render("invitations/index.ejs", {allUsers})
 }
