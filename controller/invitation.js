ff036470c5767578e86eb52a6b0ac994fdd93ff4
Fatema-Abdulla <Darksweet202@gmail.com>
2025-10-05 22:31:30 +0300

    complete 90% for create invitation

--- controller/invitation.js
+++ controller/invitation.js
@@ -1,13 +1,19 @@
 const session = require("express-session")
-const Invitation = require("../models/invitation")
 const User = require("../models/user")
 const Event = require("../models/event")
+const invitationOrganizer = require("../models/info_invitation")
 
-exports.invitation_index_get = async (req, res) => {
+exports.invitation_new_get = async (req, res) => {
   // reference https://stackoverflow.com/questions/4299991/how-to-sort-in-mongoose
   const users = await User.find({}).sort('username')
   const currentUser = req.session.user._id
-
   const allUsers = users.filter((user) => user._id.toString() !== currentUser.toString())
-  res.render("invitations/index.ejs", {allUsers})
+
+  const event = await Event.findById(req.params.eventId)
+  res.render("invitations/new.ejs", {allUsers, event})
+}
+
+exports.invitation_new_post = async (req, res) => {
+  const infoInvitation = await invitationOrganizer.create(req.body)
+  res.render("../views/index.ejs")
 }
