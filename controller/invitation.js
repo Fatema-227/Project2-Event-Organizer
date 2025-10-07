f0ee1512f8a664d3a7567fed049ea14c2d7827d7
Fatema-Abdulla <Darksweet202@gmail.com>
2025-10-07 11:05:17 +0300

    work at delete and send invitation

--- controller/invitation.js
+++ controller/invitation.js
@@ -32,6 +32,11 @@ exports.invitation_show_get = async (req, res) => {
   res.render("invitations/show.ejs", {invitations})
 }
 
+exports.invitation_delete_delete= async(req,res)=>{
+  const event = await invitationOrganizer.findByIdAndDelete(req.params.invitationId)
+  res.redirect("/invitations")
+}
+
 exports.invitation_edit_get = async (req, res) => {
   const invitations = await invitationOrganizer.findById(req.params.invitationId).populate("event_id").populate("guests")
   const users = await User.find({}).sort('username')
@@ -55,10 +60,6 @@ exports.invitation_edit_put = async (req, res) => {
       {$push: {guests: newGuests}}
     )
   }
-  res.redirect("/")
+  res.redirect(`/invitations/${invitationId}`)
 }
 
-exports.invitation_send_post = async (req, res) => {
-  const invitations = await invitationOrganizer.findById(req.params.eventId).populate("event_id").populate("guests")
-  res.render("invitations/send.ejs", {invitations})
-}
--- routes/invitation.js
+++ routes/invitation.js
@@ -7,6 +7,7 @@ router.get('/', invitationCtrl.invitation_index_get)
 router.get('/:eventId/new', invitationCtrl.invitation_new_get)
 router.post('/', invitationCtrl.invitation_new_post)
 router.get("/:eventId", invitationCtrl.invitation_show_get)
+router.get("/:eventId", invitationCtrl.invitation_delete_delete)
 router.get('/edit/:invitationId', invitationCtrl.invitation_edit_get)
 router.put('/edit/:invitationId', invitationCtrl.invitation_edit_put)
 
