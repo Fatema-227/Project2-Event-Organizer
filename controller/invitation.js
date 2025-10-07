c0bf2e8ab200bbdde1c4152c3515cd26b68ff875
Fatema-Abdulla <Darksweet202@gmail.com>
2025-10-07 23:27:25 +0300

    finish send invitation for guests

--- controller/invitation.js
+++ controller/invitation.js
@@ -2,12 +2,13 @@ const session = require("express-session")
 const User = require("../models/user")
 const Event = require("../models/event")
 const invitationOrganizer = require("../models/info_invitation")
+const invitation = require("../models/invitation")
 
 exports.invitation_index_get = async (req, res) => {
   const userId = req.session.user._id
   // reference: https://stackoverflow.com/questions/12821596/multiple-populates-mongoosejs
-  const invitations = await invitationOrganizer.find({user_id: userId}).populate("user_id").populate("event_id")
-  res.render("invitations/index.ejs", {invitations})
+  const invitations = await invitationOrganizer.find().populate("event_id").populate("guests")
+  res.render("invitations/index.ejs", {invitations, userId})
 }
 
 exports.invitation_new_get = async (req, res) => {
@@ -28,8 +29,31 @@ exports.invitation_new_post = async (req, res) => {
 }
 
 exports.invitation_show_get = async (req, res) => {
-  const invitations = await invitationOrganizer.findById(req.params.eventId).populate("event_id").populate("guests")
-  res.render("invitations/show.ejs", {invitations})
+  const invitations = await invitationOrganizer.findById(req.params.invitationId).populate("event_id").populate("guests")
+  const currentUser = req.session.user._id
+  res.render("invitations/show.ejs", {invitations, currentUser})
+}
+
+exports.invitation_show_guest_get = async (req, res) => {
+  const invitations = await invitationOrganizer.findById(req.params.invitationId).populate("event_id").populate("guests")
+  const currentUser = req.session.user._id
+  // reference: https://www.mongodb.com/docs/manual/reference/method/db.collection.findOne/
+  const invitationResponse = await invitation.findOne({
+    guest_id: currentUser,
+    invitationCreated_id: req.params.invitationId
+  })
+  res.render("invitations/showguest.ejs", {invitations, currentUser, invitationResponse})
+}
+
+exports.invitation_show_guest_post = async (req, res) => {
+  const invitationsId = await invitationOrganizer.findById(req.params.invitationId).populate("event_id").populate("guests")
+  const userId = req.session.user._id
+  req.body.invitationCreated_id = invitationsId
+  req.body.guest_id = userId
+  await invitation.create(req.body)
+
+  const invitations = await invitationOrganizer.find().populate("event_id").populate("guests")
+  res.render("invitations/index.ejs", { invitations, userId})
 }
 
 exports.invitation_edit_get = async (req, res) => {
@@ -64,4 +88,18 @@ exports.invitation_guest_delete= async(req,res)=>{
   const invitations = await invitationOrganizer.findByIdAndUpdate(invitationId,
       {$pull: {guests: deleteGuests}})
   res.redirect('/invitations')
+}
+
+exports.invitation_send_get = async (req, res) => {
+  const invitations = await invitationOrganizer.findById(req.params.invitationId)
+  res.render("invitations/send.ejs", {invitations})
+}
+
+exports.invitation_send_post = async (req, res) => {
+  const invitations = await invitationOrganizer.findById(req.params.invitationId)
+  if(invitations){
+    invitations.sendInvitation = "Sent"
+    await invitations.save()
+  }
+  res.render("invitations/send.ejs")
 }
--- routes/invitation.js
+++ routes/invitation.js
@@ -6,10 +6,17 @@ const invitationCtrl = require('../controller/invitation')
 router.get('/', invitationCtrl.invitation_index_get)
 router.get('/:eventId/new', invitationCtrl.invitation_new_get)
 router.post('/', invitationCtrl.invitation_new_post)
-router.get("/:eventId", invitationCtrl.invitation_show_get)
+router.get("/:invitationId", invitationCtrl.invitation_show_get)
 router.get('/edit/:invitationId', invitationCtrl.invitation_edit_get)
 router.put('/edit/:invitationId', invitationCtrl.invitation_edit_put)
 router.delete("/:invitationId", invitationCtrl.invitation_delete_delete)
+router.delete("/:invitationId", invitationCtrl.invitation_delete_delete)
+
+router.get("/showguest/:invitationId", invitationCtrl.invitation_show_guest_get)
+router.post("/showguest/:invitationId", invitationCtrl.invitation_show_guest_post)
+
+router.get("/", invitationCtrl.invitation_send_get)
+router.post("/:invitationId", invitationCtrl.invitation_send_post)
 
 router.delete("/:invitationId/:guestId", invitationCtrl.invitation_guest_delete)
 
