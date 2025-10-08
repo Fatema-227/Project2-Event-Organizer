6596901d789b656db253ebcc5b8c6a8f8c77cf54
fatima-mohmd <fatoomm473@gmail.com>
2025-10-08 16:14:53 +0300

    finished

--- controller/event.js
+++ controller/event.js
@@ -1,5 +1,5 @@
 const Event = require("../models/event")
-const User = require("../models/user")
+
 exports.event_index_get = async (req, res) => {
   const publicEvent = await Event.find().populate("user_id")
   const events = await Event.find({ user_id: req.session.user._id }).populate(
@@ -17,7 +17,6 @@ exports.event_index_get = async (req, res) => {
       eventDateTime.setSeconds(0)
       eventDateTime.setMilliseconds(0)
     }
-
     if (now > eventDateTime && ev.eventStatus !== "Completed") {
       ev.eventStatus = "Completed"
       await ev.save()
@@ -25,21 +24,18 @@ exports.event_index_get = async (req, res) => {
   }
   res.render("events/index.ejs", { events, publicEvent, userInSession })
 }
+
 exports.event_new_get = async (req, res) => {
   res.render("events/new.ejs")
 }
+
 exports.event_new_post = async (req, res) => {
-  const events = await Event.find({ user_id: req.session.user._id }).populate(
-    "user_id"
-  )
-  req.body.user_id = req.session.user._id
   const newEventData = {
     ...req.body,
     user_id: req.session.user._id,
     isPublic: req.body.isPublic === "on" ? true : false,
   }
   const newEvent = await Event.create(newEventData)
-
   if (req.body.submitType === "draft") {
     return res.redirect(`/events`)
   }
@@ -47,20 +43,22 @@ exports.event_new_post = async (req, res) => {
     return res.redirect(`/invitations/${newEvent._id}/new`)
   }
 }
+
 exports.event_show_get = async (req, res) => {
-  const events = await Event.findById(req.params.eventID).populate("user_id")
+  const events = await Event.findById(req.params.eventID)
   const currentUser = req.session.user._id
   const userHasBooked = events.bookedByUsers.some((event) =>
     event.equals(currentUser)
   )
   res.render("events/show.ejs", { events, currentUser, userHasBooked })
 }
+
 exports.events_show_postInvitation = async (req, res) => {
   const event = await Event.findById(req.params.eventID)
   await event.save()
-
   res.redirect(`/invitations/${event._id}/new`)
 }
+
 exports.events_book_post = async (req, res) => {
   const event = await Event.findById(req.params.eventID)
   if (event) {
@@ -70,13 +68,13 @@ exports.events_book_post = async (req, res) => {
     }
   }
   await Event.findByIdAndUpdate(req.params.eventID, {
-    $push: { bookedByUsers: req.params.userID }, //$push because it's an array with reference
+    $push: { bookedByUsers: req.params.userID },
   })
   res.redirect(`/events/${req.params.eventID}`)
 }
+
 exports.book_delete = async (req, res) => {
   const event = await Event.findById(req.params.eventID)
-
   if (event) {
     event.limit = event.limit + 1
     await event.save()
@@ -86,32 +84,21 @@ exports.book_delete = async (req, res) => {
   })
   res.redirect(`/events/${req.params.eventID}`)
 }
-// exports.events_book_get = async (req, res) => {
-//   const events = await Event.findById(req.params.eventID)
-//   if(req.params.booked){
-//     events.limit = req.params.seats -1
-//   } else{
-//     events.limit = req.params.seats +1
-//   }
-//   await events.save()
-//   console.log(events.limit)
-//   res.redirect(`/events/${req.params.eventID}`)
-// }
+
 exports.event_delete_delete = async (req, res) => {
-  const event = await Event.findByIdAndDelete(req.params.eventID)
+  await Event.findByIdAndDelete(req.params.eventID)
   res.redirect("/events")
 }
+
 exports.event_edit_get = async (req, res) => {
   const event = await Event.findById(req.params.eventID)
   res.render("events/edit.ejs", { event })
 }
+
 exports.event_edit_put = async (req, res) => {
   req.body.isPublic = req.body.isPublic === "on" ? true : false
   if (!req.body.isPublic) {
     req.body.limit = null
-  }
-  if (req.body.dateTime) {
-    req.body.dateTime = new Date(req.body.dateTime)
   }
   await Event.findByIdAndUpdate(req.params.eventID, req.body)
   res.redirect(`/events/${req.params.eventID}`)
--- models/invitation.js
+++ models/invitation.js
@@ -8,11 +8,6 @@ const invitationSchema = new mongoose.Schema({
     type: String,
     enum: ["Pending", "Accepted", "Declined", "Cancelled/ Completed"]
   },
-  userEvent_id: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'UserEvent',
-    required: true
-  },
     invitationCreated_id: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'InvitationOrganizer',
